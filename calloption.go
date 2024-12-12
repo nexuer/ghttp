616d0293aaece9f13c39fa70c9961741341f02e9
zdz1715 <zdzserver@163.com>
2024-12-12 18:39:55 +0800

    add many callOption

--- calloption.go
+++ calloption.go
@@ -2,8 +2,9 @@ package ghttp
 
 import (
 	"context"
-	"github.com/nexuer/ghttp/query"
 	"net/http"
+
+	"github.com/nexuer/ghttp/query"
 )
 
 type Limiter interface {
@@ -13,6 +14,64 @@ type Limiter interface {
 type CallOption interface {
 	Before(request *http.Request) error
 	After(response *http.Response) error
+}
+
+func setQuery(req *http.Request, q any) error {
+	if q == nil {
+		return nil
+	}
+	values, err := query.Values(q)
+	if err != nil {
+		return err
+	}
+	queryStr := values.Encode()
+	if queryStr == "" {
+		return nil
+	}
+
+	if req.URL.RawQuery == "" {
+		req.URL.RawQuery = queryStr
+	} else {
+		req.URL.RawQuery += "&" + queryStr
+	}
+	return nil
+}
+
+func Query(q any) CallOption {
+	return queryCallOption{query: q}
+}
+
+type queryCallOption struct {
+	query any
+}
+
+func (q queryCallOption) Before(request *http.Request) error {
+	return setQuery(request, q.query)
+}
+
+func (q queryCallOption) After(response *http.Response) error {
+	return nil
+}
+
+func BasicAuth(username, password string) CallOption {
+	return basicAuthCallOption{username, password}
+}
+
+type basicAuthCallOption struct {
+	username string
+	password string
+}
+
+func (b basicAuthCallOption) Before(request *http.Request) error {
+	request.SetBasicAuth(b.username, b.password)
+	if b.username != "" || b.password != "" {
+		request.SetBasicAuth(b.username, b.password)
+	}
+	return nil
+}
+
+func (b basicAuthCallOption) After(response *http.Response) error {
+	return nil
 }
 
 // CallOptions default call options
@@ -38,19 +97,9 @@ func (c *CallOptions) Before(request *http.Request) error {
 			return err
 		}
 	}
-	if c.Query != nil {
-		values, err := query.Values(c.Query)
-		if err != nil {
-			return err
-		}
-		q := values.Encode()
-		if q != "" {
-			if request.URL.RawQuery == "" {
-				request.URL.RawQuery = q
-			} else {
-				request.URL.RawQuery += "&" + q
-			}
-		}
+
+	if err := setQuery(request, c.Query); err != nil {
+		return err
 	}
 
 	if c.Username != "" || c.Password != "" {
